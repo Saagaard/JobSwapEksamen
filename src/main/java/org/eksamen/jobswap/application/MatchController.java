e41f29504557d72a812ac900c76bd55307866a0a
Saagaard <85276934+Saagaard@users.noreply.github.com>
2025-05-21 14:52:19 +0200

    Implemented Criteria search

--- src/main/java/org/eksamen/jobswap/application/MatchController.java
+++ src/main/java/org/eksamen/jobswap/application/MatchController.java
@@ -1,6 +1,15 @@
 package org.eksamen.jobswap.application;
 
+import org.eksamen.jobswap.domain.Match;
+
+import java.util.List;
+
 public class MatchController {
+    List<Match> matches;
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
+    }
 
 
 }
--- src/main/java/org/eksamen/jobswap/application/SearchController.java
+++ src/main/java/org/eksamen/jobswap/application/SearchController.java
@@ -9,8 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.eksamen.jobswap.domain.Criteria;
-import org.eksamen.jobswap.domain.Employee;
-import org.eksamen.jobswap.persistence.EmployeeDAOImpl;
+import org.eksamen.jobswap.domain.Match;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,31 +18,57 @@ public class SearchController {
     @FXML
     private TextField jobTitleField, transportTimeField, salaryField, minimumYearField, minimumMonthField, maxYearField, maxMonthField;
 
-    public void initialize() throws Exception {
-        EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
-        List<Employee> testEmployeeList = employeeDAO.readAll();
+    Criteria criteria;
 
-        for (Employee employee : testEmployeeList) {
-            System.out.println(employee.getFirstName());
-            System.out.println(employee.getHomeAddressZip().getCityName());
-        }
+    public void initialize() throws Exception {
+
     }
 
-    public void generateCriterias() {
+    public void generateCriteria() {
 
+        String jobTitle =  jobTitleField.getText();
+        int transportTime = Integer.parseInt(transportTimeField.getText());
+        float salary = Float.parseFloat(salaryField.getText());
 
+        int minimumYear = Integer.parseInt(minimumYearField.getText());
+        int minimumMonth = Integer.parseInt(minimumMonthField.getText());
+        int minimumSeniority = minimumYear * 12 + minimumMonth;
 
+        int maxYear = Integer.parseInt(maxYearField.getText());
+        int maxMonth = Integer.parseInt(maxMonthField.getText());
+        int maxSeniority = maxYear * 12 + maxMonth;
+
+        this.criteria = new Criteria(jobTitle, transportTime, salary, minimumSeniority, maxSeniority);
+        searchMatches(this.criteria);
+
+    }
+
+    public List<Match> searchMatches(Criteria criteria) {
+        System.out.println("Jobtitel: " + criteria.getJobTitle());
+        System.out.println("Minimum anciennitet: " + criteria.getMinimumSeniority());
+        System.out.println("Max anciennitet: " + criteria.getMaxSeniority());
+        System.out.println("Lønafvigelse: " + criteria.getSalaryDifference());
+        System.out.println("Ny transporttid: " + criteria.getTransportTime());
+
+        //        MatchController matchController = loader.getController();
+        //menuController.setMatches(List<Match>);
+
+        return null;
     }
 
 
     public void switchToMatchMenu(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/eksamen/jobswap/ui/match.fxml"));
-        Parent root = loader.load();
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/org/eksamen/jobswap/ui/match.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
+
+
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/eksamen/jobswap/ui/match.fxml"));
+//        Parent root = loader.load();
+//
+//
+//        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("/org/eksamen/jobswap/ui/match.css").toExternalForm());
+//        stage.setScene(scene);
+//        stage.show();
     }
 }
