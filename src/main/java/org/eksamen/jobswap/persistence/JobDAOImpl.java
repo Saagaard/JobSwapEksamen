2b856accce993cd72b9ef5dd273793936e7ae2f8
Winnex <123577748+TheRealWinnex@users.noreply.github.com>
2025-05-21 14:44:39 +0200

    W.I.P på JobDAOImpl.java

--- src/main/java/org/eksamen/jobswap/persistence/JobDAOImpl.java
+++ src/main/java/org/eksamen/jobswap/persistence/JobDAOImpl.java
@@ -1,7 +1,12 @@
 package org.eksamen.jobswap.persistence;
 
 import org.eksamen.jobswap.domain.Job;
-
+import org.eksamen.jobswap.foundation.SqlConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JobDAOImpl implements JobDAO {
@@ -14,8 +19,33 @@ public class JobDAOImpl implements JobDAO {
         return null;
     }
 
-    public List<Job> readAll() {
-        return null;
+    public List<Job> readAll() throws Exception {
+        List<Job> jobs = new ArrayList<>();
+        String sql = "SELECT * FROM dbo.tblJob";
+
+        //try-with-resources lukker automatisk ResultSet
+        try (
+                Connection conn = SqlConnection.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery()
+        ) {
+            while (rs.next()) {
+                int jobID = rs.getInt("fldJobID");
+                int employeeID = rs.getInt("fldEmployeeID");
+                int workplaceID = rs.getInt("fldWorkPlaceID");
+                String jobTitle = rs.getString("fldJobTitle");
+                LocalDate employmentDate = rs.getDate("fldEmploymentDate").toLocalDate();
+                float monthlySalary = rs.getFloat("fldMonthlySalary");
+
+                jobs.add(new Job(jobID, employee, workplace, jobTitle, employmentDate, monthlySalary));
+            }
+
+            if (jobs.isEmpty()) {
+                System.out.println("Der blev ikke fundet nogle jobs");
+            }
+
+            return jobs;
+        }
     }
 
     public void update(Job job) {
