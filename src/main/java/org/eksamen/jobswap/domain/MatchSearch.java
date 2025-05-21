7d84d9fcda3379010327fda796d33ef0b2ed7f20
Winnex <123577748+TheRealWinnex@users.noreply.github.com>
2025-05-21 14:44:07 +0200

    W.I.P matchsearch algorithm

--- src/main/java/org/eksamen/jobswap/domain/MatchSearch.java
+++ src/main/java/org/eksamen/jobswap/domain/MatchSearch.java
@@ -1,5 +1,9 @@
 package org.eksamen.jobswap.domain;
+import org.eksamen.jobswap.persistence.EmployeeDAOImpl;
+import org.eksamen.jobswap.persistence.JobDAOImpl;
+import org.eksamen.jobswap.persistence.WorkplaceDAOImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatchSearch {
@@ -8,4 +12,23 @@ public class MatchSearch {
         return null;
     }
 
+    public static void employeeList() throws Exception {
+        EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl(); // initialisér employeeDAOImpl objektet.
+        List<Employee> testEmployeeList = employeeDAO.readAll(); // Lav en arrayliste, og kald readAll() metoden på objektet for at fylde listen med data
+
+        JobDAOImpl jobDAO = new JobDAOImpl();
+        List<Job> testJobList = jobDAO.readAll();
+
+        WorkplaceDAOImpl workplaceDAO = new WorkplaceDAOImpl();
+        List<Workplace> testWorkplaceList = workplaceDAO.readAll();
+
+        List<Match> matchList = new ArrayList<>();
+
+        //samle al data i en ny liste "matches" så vi kan regne med API metoden og videresende al data til match cards.
+    }
+
+    public static void main(String[] args) throws Exception {
+        employeeList();
+
+    }
 }
