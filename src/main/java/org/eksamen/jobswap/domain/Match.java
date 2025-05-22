c28c4cb0e16615e3664f353f9272e473daac10a1
Saagaard <85276934+Saagaard@users.noreply.github.com>
2025-05-22 12:07:00 +0200

    Implemented match-algorithm. Changed Match to contain Job instead of Employee

--- src/main/java/org/eksamen/jobswap/domain/Match.java
+++ src/main/java/org/eksamen/jobswap/domain/Match.java
@@ -1,31 +1,31 @@
 package org.eksamen.jobswap.domain;
 
 public class Match {
-    Employee employee1;
-    int employee1NewTransportTime;
-    Employee employee2;
-    int employee2NewTransportTime;
+    Job job1;
+    int job1NewTransportTime;
+    Job job2;
+    int job2NewTransportTime;
 
-    public Match(Employee employee1, int employee1NewTransportTime, Employee employee2, int employee2NewTransportTime) {
-        this.employee1 = employee1;
-        this.employee1NewTransportTime = employee1NewTransportTime;
-        this.employee2 = employee2;
-        this.employee2NewTransportTime = employee2NewTransportTime;
+    public Match(Job job1, int job1NewTransportTime, Job job2, int job2NewTransportTime) {
+        this.job1 = job1;
+        this.job1NewTransportTime = job1NewTransportTime;
+        this.job2 = job2;
+        this.job2NewTransportTime = job2NewTransportTime;
     }
 
-    public Employee getEmployee1() {
-        return employee1;
+    public Job getJob1() {
+        return job1;
     }
 
-    public int getEmployee1NewTransportTime() {
-        return employee1NewTransportTime;
+    public int getJob1NewTransportTime() {
+        return job1NewTransportTime;
     }
 
-    public Employee getEmployee2() {
-        return employee2;
+    public Job getJob2() {
+        return job2;
     }
 
-    public int getEmployee2NewTransportTime() {
-        return employee2NewTransportTime;
+    public int getJob2NewTransportTime() {
+        return job2NewTransportTime;
     }
 }
--- src/main/java/org/eksamen/jobswap/domain/MatchSearch.java
+++ src/main/java/org/eksamen/jobswap/domain/MatchSearch.java
@@ -6,31 +6,90 @@ import org.eksamen.jobswap.persistence.WorkplaceDAOImpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.abs;
+
 public class MatchSearch {
 
-    public List<Match> createMatches(Criteria criteria) {
-        return null;
+    public static List<Match> createMatches(Criteria criteria) throws Exception {
+        JobDAOImpl jobDAO = new JobDAOImpl();
+        List<Job> jobList = jobDAO.readAll();
+        List<Match> matchList = new ArrayList<>();
+
+        for (int job1Index = 0; job1Index < jobList.size(); job1Index++) {
+            Job job1 = jobList.get(job1Index);
+            System.out.println("ny job1 iteration");
+
+            for (int i = 0; i < jobList.size(); i++) {
+                System.out.println("ny job2 iteration");
+                if (i == job1Index) { continue; } // Now we are correctly skipping matching the same job
+                Job job2 = jobList.get(i);
+
+                // Jobtitel
+                if (!criteria.getJobTitle().isEmpty()) {
+                    if (!criteria.getJobTitle().equals(job1.getJobTitle()) || !criteria.getJobTitle().equals(job2.getJobTitle())) {
+                        System.out.println("Jobtitel matcher IKKE");
+                        continue;
+                    } else {
+                        System.out.println("jobtitel matcher");
+                    }
+                } else {
+                    System.out.println("Ingen jobtitel valgt");
+                }
+
+
+//                    if (!criteria.getTransportTime() > Distance.getTransportDetails(job1.employee.getHomeAddress(), job2.workplace.getWorkAddress())
+//                            &&
+//                        !criteria.getTransportTime() > Distance.getTransportDetails(job2.employee.getHomeAddress(), job1.workplace.getWorkAddress()))
+//                    {
+//
+//                    }
+
+                // Lønafvigelse
+                System.out.println("salary difference: " +  abs(job1.getMonthlySalary() - job2.getMonthlySalary()));
+                if (criteria.getSalaryDifference() > abs(job1.getMonthlySalary() - job2.getMonthlySalary())) {
+                    System.out.println("Lønafvigelse matcher");
+                } else {
+                    System.out.println("Lønafvigelse matcher IKKE");
+                    continue;
+                }
+
+                // Minimum og max anciennitet
+                System.out.println("Job1 anciennitet: " + job1.calculateSeniority() + " Job2 anciennitet: " + job2.calculateSeniority());
+
+                if (job1.calculateSeniority() > criteria.getMinimumSeniority() && job2.calculateSeniority() > criteria.getMinimumSeniority()
+                    && job1.calculateSeniority() < criteria.getMaxSeniority() && job2.calculateSeniority() < criteria.getMaxSeniority()){
+
+                    System.out.println("Minimum og max anciennitet matcher");
+                } else {
+                    System.out.println("Minimum og max anciennitet matcher IKKE");
+                    continue;
+                }
+
+                System.out.println("Match fundet!!!!!!");
+                matchList.add(new Match (job1, 20, job2, 20));
+
+            }
+
+        }
+        return matchList;
     }
 
     public static void employeeList() throws Exception {
 
         JobDAOImpl jobDAO = new JobDAOImpl();
         List<Job> testJobList = jobDAO.readAll();
-        for (Job job : testJobList) {
 
-            for (int i = 0; i <= testJobList.size(); i++) {
-
-            }
-
-        }
-
-        List<Match> matchList = new ArrayList<>();
 
         //samle al data i en ny liste "matches" så vi kan regne med API metoden og videresende al data til match cards.
     }
 
     public static void main(String[] args) throws Exception {
-        employeeList();
+        List<Match> matchList = createMatches(new Criteria("", 20, 5000, 12, 100));
+//        System.out.println("Matches:");
+//        for (Match match : matchList) {
+//            System.out.println(match.job1.getEmployee().getFirstName() + " " + match.job1.getEmployee().getLastName());
+//            System.out.println(match.job2.getEmployee().getFirstName() + " " + match.job2.getEmployee().getLastName());
+//        }
 
     }
 }
