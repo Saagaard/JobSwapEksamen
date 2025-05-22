0e4798345f2a796997bec7a38e2c9f76c3f53b79
Saagaard <85276934+Saagaard@users.noreply.github.com>
2025-05-22 13:50:42 +0200

    Fixed JobSwapApplication fxml-path

--- src/main/java/org/eksamen/jobswap/application/JobSwapApplication.java
+++ src/main/java/org/eksamen/jobswap/application/JobSwapApplication.java
@@ -1,4 +1,4 @@
-package org.eksamen.jobswap;
+package org.eksamen.jobswap.application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class JobSwapApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JobSwapApplication.class.getResource("UI/MatchDetails.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(JobSwapApplication.class.getResource("/org/eksamen/jobswap/ui/MatchDetails.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
