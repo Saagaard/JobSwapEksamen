1a0d58b5fff351d25dd05975c0f95f1d95c7808e
saagaard <simonvangaagaard@gmail.com>
2025-05-30 11:08:02 +0200

    Removed a comment

--- src/main/java/org/eksamen/jobswap/businessServices/CalculateSalaryDifference.java
+++ src/main/java/org/eksamen/jobswap/businessServices/CalculateSalaryDifference.java
@@ -17,7 +17,6 @@ public class CalculateSalaryDifference {
     public static float calculateSalaryDifference(Job job1, Job job2) {
         float salaryDifference = abs(job1.getMonthlySalary() - job2.getMonthlySalary());
         float percentageIncrease = (salaryDifference / job1.getMonthlySalary()) * 100;
-        //System.out.println("Procent stigning løn %: " + percentageIncrease);
         return percentageIncrease;
     }
 }
